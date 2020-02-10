861a907343cd768bdbbd8a8ec60195c82818b6ee
Aleksandar <rachev.96@abv.bg>
2020-02-10 13:23:33 +0200

    created project

---
+++ src/main/java/unwe/register/UnweRegister/UnweRegisterApplication.java
@@ -0,0 +1,13 @@
+package unwe.register.UnweRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnweRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnweRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unwe/register/UnweRegister/UnweRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package unwe.register.UnweRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnweRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
